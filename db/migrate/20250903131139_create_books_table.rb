c009dd20ad4231e5adb2f4c67cbb229a350203b4
Pedro Ariztia <ppariztia1@miuandes.cl>
2025-09-03 09:22:43 -0400

    migration adding books table

--- db/migrate/20250903131139_create_books_table.rb
+++ db/migrate/20250903131139_create_books_table.rb
@@ -1,6 +1,9 @@
 class CreateBooksTable < ActiveRecord::Migration[8.0]
   def change
     create_table :books_tables do |t|
+      t.string :title, null: false
+      t.date :publication_date, null: false
+      
       t.timestamps
     end
   end
