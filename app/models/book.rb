f9e19e0e2a0e375d1cff678b66c300f1331f9bc1
Pedro Ariztia <ppariztia1@miuandes.cl>
2025-09-17 09:45:59 -0300

    validaciones

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
     belongs_to :author
+
+    validates :title, presence: true, uniqueness: true
+    # Ponemos que se valide para la integridad de los datos:
+    # para que el libro sea valido el libro debe tener titulo
+    # y que no haya otro libro con el mismo titulo
 end
