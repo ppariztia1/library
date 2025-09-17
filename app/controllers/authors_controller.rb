b76ad6d6d380bacc04f86f13da2d5570f80ca913
Pedro Ariztia <ppariztia1@miuandes.cl>
2025-09-17 10:18:50 -0300

    fin clase 5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorController < ApplicationController
+  before_action :set_author, only: [:show] # No queremos que se ejecute en el index
+
   def index
     # Primero debo ir a la base de datos para extraer los autores
     # Y el arroba indica que es una variable global, es decir, que funciona para toda la class
@@ -6,6 +8,11 @@ class AuthorController < ApplicationController
   end
 
   def show 
+  end
+
+ # Este callback sería en el caso de que tenga el update y destroy, para no tener que repetir la linea de codigo.
+  def set_author
     @author = Author.find(params[:id])
   end
+
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,25 @@
 class Author < ApplicationRecord
     has_many :books
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    
+    # Callbacks: antes de la validacion que haga esto, sino, que falle
+    before_validation :strip_name
+    before_validation :titleize_name
+
+
     def full_name
         "#{first_name} #{last_name}"
+    end
+
+    def strip_name
+        self.first_name = first_name.strip
+        self.last_name = last_name.strip
+    end
+
+    def titleize_name # Pone la primera letra en mayuscula
+        self.first_name = first_name.titleize
+        self.last_name = last_name.titleize
     end
 end
